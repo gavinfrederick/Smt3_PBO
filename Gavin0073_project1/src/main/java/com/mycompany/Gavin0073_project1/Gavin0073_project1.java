95593fd434a13b235eb6954de95cf25d46850bbe
Gavin Frederick Purnomo <gavinpurnomo@gmail.com>
2026-08-31 21:35:04 +0700

    Fix nested git cache and track files properly

---
+++ Gavin0073_project1/src/main/java/com/mycompany/Gavin0073_project1/Gavin0073_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gavin0073_project1;
+
+/**
+ *
+ * @author Hp Victus
+ */
+public class Gavin0073_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("NAMA    :GAVIN");
+        System.out.println("NPM     :2505060068");
+        System.out.println("ALAMAT  :KARANGGADING WEL");
+        System.out.println("NO_HP   :08516612999");
+    }
+}
